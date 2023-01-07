742150ac18c7419b1a6648eeb8bc753c623b3fd7
soundarkeshav <122149960+soundarkeshav@users.noreply.github.com>
2023-01-07 16:57:59 +0530

    Update LineChart.js

--- LineChart.js
+++ LineChart.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactApexChart from 'react-apexcharts';
 
-class LineChart extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {          
-        series: [{
-          name: 'Website Blog',
-          type: 'area',
-          data: []
-        }],
-        options: {
+export default function LineChart() {
+    
+  const [series, setSeries] = React.useState();
+
+  const [options, setOptions] = React.useState();
+
+  const [showChart, setShowChart] = React.useState();
+       
+
+  React.useEffect(() => {
+    setSeries({series:
+      [{
+          name: 'Current Indicator',
+          type: 'column',
+          data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+        }, {
+          name: 'Average',
+          type: 'line',
+          data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+        }]}
+      );
+
+      setOptions({
           chart: {
             height: 350,
             type: 'line',
@@ -19,31 +32,36 @@ class LineChart extends React.Component {
             width: [0, 4]
           },
           title: {
-            text: 'Traffic Sources'
+            text: 'Current Indicator Chart'
           },
           dataLabels: {
             enabled: true,
             enabledOnSeries: [1]
           },
-          labels: [],
+          labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
           xaxis: {
             type: 'datetime'
           },
           yaxis: [{
             title: {
-              text: 'Indicator',
+              text: 'Current Indicator',
             },
+          
           }]
         },
-      };
-    }
+      )
+    },[]);
 
-    render() {
+    React.useEffect(() => {
+      if(series){
+        console.log(series, options)
+        setShowChart(true)
+      }
+    },[showChart])
+
       return (
         <div id="chart">
-          <ReactApexChart options={this.state.options} series={this.state.series} type="line" height={350} />
+          {showChart && <ReactApexChart options={options} series={series.series} type="line" height={350} />}
         </div> 
       );
-    }
 }
-export default LineChart;

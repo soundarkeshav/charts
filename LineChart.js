d446da3ead7ebf9c97daf9731df73fc7913694eb
soundarkeshav <122149960+soundarkeshav@users.noreply.github.com>
2023-01-07 18:41:45 +0530

    Update LineChart.js

--- LineChart.js
+++ LineChart.js
@@ -1,67 +1,50 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export default function LineChart() {
-    
-  const [series, setSeries] = React.useState();
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
 
-  const [options, setOptions] = React.useState();
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+];
 
-  const [showChart, setShowChart] = React.useState();
-       
-
-  React.useEffect(() => {
-    setSeries({series:
-      [{
-          name: 'Current Indicator',
-          type: 'column',
-          data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
-        }, {
-          name: 'Average',
-          type: 'line',
-          data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-        }]}
-      );
-
-      setOptions({
-          chart: {
-            height: 350,
-            type: 'line',
-          },
-          stroke: {
-            width: [0, 4]
-          },
-          title: {
-            text: 'Current Indicator Chart'
-          },
-          dataLabels: {
-            enabled: true,
-            enabledOnSeries: [1]
-          },
-          labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
-          xaxis: {
-            type: 'datetime'
-          },
-          yaxis: [{
-            title: {
-              text: 'Current Indicator',
-            },
-          
-          }]
-        },
-      )
-    },[]);
-
-    React.useEffect(() => {
-      if(series){
-        console.log(series, options)
-        setShowChart(true)
-      }
-    },[showChart])
-
-      return (
-        <div id="chart">
-          {showChart && <ReactApexChart options={options} series={series.series} type="line" height={350} />}
-        </div> 
-      );
+export default function AcccessibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="caption table">
+        <caption>A basic table example with a caption</caption>
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
